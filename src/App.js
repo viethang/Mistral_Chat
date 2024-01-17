8cbd1ac74bf477cebbccfd7ee4b199ab4ce45062
Viet Hang Nguyen <viethang@transformer>
2024-01-17 17:16:23 +0100

    Initial implementation of Chat app with Mistral API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import MistralClient from '@mistralai/mistralai';
+import { marked } from 'marked';
+import "./App.css";
 function App() {
+  const [question, setQuestion] = useState('');
+  const [response, setResponse] = useState('');
+  let parsedResponse = marked.parse(response, {});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let streamedResponse = '';
+    const client = new MistralClient('', "http://localhost:8000");
+
+    const chatStreamResponse = await client.chatStream({
+      model: 'mistralai/Mistral-7B-Instruct-v0.2',
+      messages: [{role: 'user', content: question}],
+    });
+
+    for await (const chunk of chatStreamResponse) {
+      if (chunk.choices[0].delta.content !== undefined) {
+        const streamText = chunk.choices[0].delta.content;
+        streamedResponse += streamText;
+        setResponse(streamedResponse);
+      }
+    }
+
+    setQuestion('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chat with MistralAI</h1>
       </header>
+      <main className='app-main'>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            style={{width: "300px", height: "60px"}}
+            type="text"
+            value={question}
+            onChange={(event) => setQuestion(event.target.value)}
+            placeholder="Enter your question here..."
+          />
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+        <div>
+          <h2>Response:</h2>
+          <div dangerouslySetInnerHTML={{__html: parsedResponse}}></div>
+        </div>
+      </main>
     </div>
   );
 }
